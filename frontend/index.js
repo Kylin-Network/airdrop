2f7bb5a017717e9080287de45f63d7c587e739e7
inodelisia <inodelisia@gmail.com>
2022-08-31 11:52:56 +0300

    Adding confirmation box

--- frontend/index.js
+++ frontend/index.js
@@ -187,12 +187,85 @@ async function postData(formattedFormData){
     
     const data = await response.text();
     //This should now print out the values that we sent to the backend-side
-    if (data.includes('success')) {
-        $('#messages').removeClass('hidden').addClass('alert alert-success ');
-        $('#messages_content').html('Airdrop submission successful!');
-    }
+    
+        Confirm.open({
+            title: 'Your Airdrop request has been received!',
+            message: data,
+            onok: () => {
+              document.location.href = "https://kylin.network";
+            }
+          });
+    
 
     console.log(data);
 }
+
+const Confirm = {
+    open (options) {
+        options = Object.assign({}, {
+            title: '',
+            message: '',
+            okText: 'OK',
+            cancelText: 'Send Another',
+            onok: function () {},
+            oncancel: function () {}
+        }, options);
+        
+        const html = `
+            <div class="confirm">
+                <div class="confirm__window">
+                    <div class="confirm__titlebar">
+                        <span class="confirm__title">${options.title}</span>
+                        <button class="confirm__close">&times;</button>
+                    </div>
+                    <div class="confirm__content">${options.message}</div>
+                    <div class="confirm__buttons">
+                        <button class="confirm__button confirm__button--ok confirm__button--fill">${options.okText}</button>
+                        <button class="confirm__button confirm__button--cancel">${options.cancelText}</button>
+                    </div>
+                </div>
+            </div>
+        `;
+
+        const template = document.createElement('template');
+        template.innerHTML = html;
+
+        // Elements
+        const confirmEl = template.content.querySelector('.confirm');
+        const btnClose = template.content.querySelector('.confirm__close');
+        const btnOk = template.content.querySelector('.confirm__button--ok');
+        const btnCancel = template.content.querySelector('.confirm__button--cancel');
+
+        confirmEl.addEventListener('click', e => {
+            if (e.target === confirmEl) {
+                options.oncancel();
+                this._close(confirmEl);
+            }
+        });
+
+        btnOk.addEventListener('click', () => {
+            options.onok();
+            this._close(confirmEl);
+        });
+
+        [btnCancel, btnClose].forEach(el => {
+            el.addEventListener('click', () => {
+                options.oncancel();
+                this._close(confirmEl);
+            });
+        });
+
+        document.body.appendChild(template.content);
+    },
+
+    _close (confirmEl) {
+        confirmEl.classList.add('confirm--close');
+
+        confirmEl.addEventListener('animationend', () => {
+            document.body.removeChild(confirmEl);
+        });
+    }
+};
+
 
 
